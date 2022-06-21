3ae97c4d5b55df950a8150017c6f1ac70a7a0156
Karnav Pargi <karnav.pargi@bytestechnolab.in>
2022-06-21 12:57:44 +0530

    Added swagger json UI and added API

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 require('dotenv').config();
-
 const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app)
 const morgan = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const http = require('http');
+const cors = require('cors');
+const app = express();
 
+const server = http.createServer(app)
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('dev'));
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Routes for the API
 app.use(require('./api/routes'));
